c37dce45b6dfb160deff5181d2f6d505e235f312
Devi30 <96772778+Devi30@users.noreply.github.com>
2022-08-06 10:55:51 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var canvas = new fabric.Canvas('myCanvas');
 function new_image()
 {
 fabric.Image.fromURL('BirthdayImage.jpg',function(img){
-    block_image_object=Img;
+    block_image_object=img;
     block_image_object.scaleToWidth(700);
     block_image_object.scaleToHeight(510);
     block_image_object.set({
